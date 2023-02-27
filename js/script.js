dc354e9894d6683bc72c122a090c360201205d10
marcodedemo <marco.dedemo@gmail.com>
2023-02-27 16:09:01 +0100

    feat: exercise 2

--- js/script.js
+++ js/script.js
@@ -69,3 +69,74 @@ submitEl.addEventListener("click", function(){
 
 })
 
+
+
+
+/* ---------------------------------------------------------------------------------- */
+
+
+/*
+
+    genero due numeri casuali tra 1 e 6 
+    confronto i due numeri
+
+    SE
+        ° il numero dell'utente è maggiore del numero del computer stampo "hai vinto"
+
+    ALTRIMENTI SE 
+        ° il numero dell'utente è minore del numero del computer stampo "hai perso"
+
+    ALTRIMENTI 
+        ° se i due numeri sono uguali stampo "parità"
+*/
+
+
+
+
+
+
+// dichiaro la variabile corrispondente al pulsante che crea un numero random
+
+let rollDices = document.getElementById("random-btn");
+
+// alla pressione del pulsante
+rollDices.addEventListener("click", function(){
+
+
+    // genero due numeri random tra 1 e 6 e li memorizzo
+    let userNumber = Math.floor(Math.random()* 6) + 1 ;
+    let pcNumber = Math.floor(Math.random()* 6) + 1 ;
+
+    // dichiaro le variabili corrispondenti alle immagini dei dadi per poi potergli cambiare le immagini
+    let userDiceEl = document.getElementById("user-dice-img");
+    let pcDiceEl = document.getElementById("pc-dice-img");
+
+    // rendo visibili le immagini dei dadi solo dopo la prima generazione randomica
+    userDiceEl.style.display = "block";
+    pcDiceEl.style.display = "block";
+
+    // attribuisco alla src dell'immagine il valore randomico generato per inserire l'immagine del dado corretto
+    userDiceEl.setAttribute("src", "img/" + userNumber + ".png");
+    pcDiceEl.setAttribute("src", "img/" + pcNumber + ".png")
+
+
+    // dichiaro la variabile corrispondente all'elemento html in cui scrivere la frase di vittoria/sconfita
+    let resultEl = document.getElementById("win-lose");
+
+
+    if(userNumber > pcNumber){
+
+        resultEl.innerHTML = "You Win!!";
+
+    } else if (userNumber < pcNumber){
+
+        resultEl.innerHTML = "You Lose!!";
+
+    } else{
+        resultEl.innerHTML = "Tie!";
+
+    }
+
+})
+
+
